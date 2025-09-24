dc4d07eccd37857bcc721440aed886768cc1a66a
Light <raynardli99@gmail.com>
2025-09-24 17:53:58 +0800

    Corrected Euler_partitions.cpp

--- Euler_partitions.cpp
+++ Euler_partitions.cpp
@@ -3,8 +3,8 @@ Author: Li J.R.
 Date: 24 Sep. 2025
 Description:
 This version of the partitions calculator for number n
-uses the a form of Euler's pentagonal number theorem,
-where 
+uses the a form of Euler's pentagonal number recurrence,
+where p(n) = ∑​(−1)^k+1(p(n−g1​)+p(n−g2​))
 */
 
 #include <iostream>
@@ -34,15 +34,28 @@ int main(void) {
 }
 
 unsigned long long partitionsCounter(int n) {
-    vector<unsigned long long> dp(n + 1);
+    vector<unsigned long long> p(n + 1);
 
-    dp[0] = 1;
+    p[0] = 1;
 
-    for (int k = 1; k <= n; k++) {
-        for (int i = k; i <= n; i++) {
-            dp[i] += dp[i - k];
+    for (int i = 1; i <= n; i++) {
+        long long total = 0;
+        for (int m = 1; ; m++) {
+
+            // Calculate g(m), the generalized pentagonal numbers
+            int g1 = m * (3 * m - 1) / 2;
+            int g2 = m * (3 * m + 1) / 2;
+            if (g1 > i) break;
+
+            int sign = (m % 2 == 0 ? -1 : 1);   // Check the sign alternation by pairs
+
+            total += sign * p[i - g1];          // assign signage to value added to total
+
+            if (g2 <= i)
+                total += sign * p[i - g2];
         }
+        p[i] = total;
     }
 
-    return dp[n];
+    return p[n];
 }
